c4ffdd99736ddf3e4ac907ecb2fa70b37cb65e08
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2024-01-17 20:15:33 +0530

    Add files via upload

--- Hyperbolic Pfaffian.py
+++ Hyperbolic Pfaffian.py
@@ -2,8 +2,9 @@ import math as m
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+from numpy.linalg import eig
 
-
+#Total number points upto generation g
 def cmax(n,g):
     if g==0:
         return n
@@ -17,13 +18,14 @@ def cmax(n,g):
             c0+=n0+n1
     return c0
 
-
+#Special treatment for g=0
 def sigma(g):
     if g==0:
         return 0
     else:
         return 1
 
+#Differing the looping part
 def f(n,c,j):
     if j==0:
        if c==n:
@@ -36,6 +38,8 @@ def f(n,c,j):
         else:
             return cmax(n,j-1)+1
 
+#Construction of the (n,3) Hyperbolic graph with Pfaffian orientation
+        
 def Hyp(n, g):
     E,Eo = [],[]  #Edges and edges with orientations
     vertices_list=[[]]
@@ -53,79 +57,76 @@ def Hyp(n, g):
             E.append((c,1))
             Eo.append([(c,1),1])
 
-    j = 1
-    while j <= g:
+    for j in range(1,g+1):
         new_vertices = []
         for v in vertices_list[-1]:
             c0=v[-1]
-            if v[-2] != 0:
+            if v[-2]!=0:
                 if v[-2]!=(n-4)*sigma(j-1):
                     for i in range(0, n-3):
                         c += 1
                         if i==0:
                            E.append((c0,c))
                            Eo.append([(c0,c),1])
-                        if c<cmax(n,g):
+                        if c<cmax(n,j):
                             nv = v[:-1]  # Remove the last element of v
                             nv.append(i)
                             nv.append(c)
                             new_vertices.append(nv)
-                            E.append((c,c+1)) #Need to put conditions 
+                            E.append((c,c+1)) # Putting conditions in order to construct Pfaffian orientation
                             sgn=Eo[E.index((c0,f(n,c0,j-1)))][1]
                             if i<int((n-3)/2)+(1+sgn)/2:
                                Eo.append([(c,c+1),1])
                             else:
                                Eo.append([(c,c+1),-1])
-                            
                         else:
                             nv = v[:-1]  # Remove the last element of v
                             nv.append(i)
                             nv.append(c)
                             new_vertices.append(nv)
-                            E.append((c,cmax(n,g-1)+1))
-                            Eo.append([(c,cmax(n,g-1)+1),-1])
-                            break
+                            E.append((c,cmax(n,j-1)+1))
+                            Eo.append([(c,cmax(n,j-1)+1),-1])
+                      
                 else:
-                    for i in range(0, n-4):
-                        c += 1
-                        if i==0:
-                           E.append((c0,c))
-                           Eo.append([(c0,c),1])
-                        if c==cmax(n,g):
-                            nv = v[:-1]  # Remove the last element of v
-                            nv.append(i)
-                            nv.append(c)
-                            new_vertices.append(nv)
-                            E.append((c,cmax(n,g-1)+1))
-                            Eo.append([(c,cmax(n,g-1)+1),-1])
-                            break
-                        else:
-                            v = v[:-1]  # Remove the last element of v
-                            nv.append(i)
-                            nv.append(c)
-                            new_vertices.append(nv)
-                            E.append((c,c+1))
-                            if i<int((n-3)/2)+(1+sgn)/2:
-                                Eo.append([(c,c+1),1])
+                    if v[-2]==(n-4)*sigma(j-1):
+                        for i in range(0, n-4):  #These are the points just to the left of an n0 point
+                            c += 1
+                            if i==0:
+                             E.append((c0,c))
+                             Eo.append([(c0,c),1])
+                            if c<cmax(n,j):
+                                nv = v[:-1]  # Remove the last element of v
+                                nv.append(i)
+                                nv.append(c)
+                                new_vertices.append(nv)
+                                E.append((c,c+1))
+                                sgn=Eo[E.index((c0,f(n,c0,j-1)))][1]
+                                if i<int((n-3)/2)+(1+sgn)/2:
+                                 Eo.append([(c,c+1),1])
+                                else:
+                                 Eo.append([(c,c+1),-1])
                             else:
-                               Eo.append([(c,c+1),-1])
+                                nv=v[:-1]  # Remove the last element of v
+                                nv.append(i)
+                                nv.append(c)
+                                new_vertices.append(nv)
+                                E.append((c,cmax(n,j-1)+1))
+                                Eo.append([(c,cmax(n,j-1)+1),-1])
+                            
         vertices_list.append(new_vertices)
-        j += 1
+
     return vertices_list, E , Eo
 
-# Example usage
-# Print the vertices of the second layer (j=1)
+#Almost there
 
-
-E,Eo=Hyp(7,2)[1],Hyp(7,2)[2]
-#E=Hyp(7,2)[1]
-G=nx.Graph(E)
-G.add_edges_from(E)
-nx.draw(G, with_labels=True)
-print(Eo)
-plt.show()
-
-#Almost correct, but a little correction is required. And need to construct Pfaffian orientation. Doit recurrsively as well.
+# a=int(input("Enter number of generations"))
+# E,Eo=Hyp(7,a)[1],Hyp(7,a)[2]
+# G=nx.Graph(E)
+# G.add_edges_from(E)
+# nx.draw(G, with_labels=True)
+# print(Eo)
+# # print(Hyp(7,a)[0])
+# plt.show()
 
 #Building the Pfaffian matrix and calculate the eigenspectrum (May be Sturm sequnce method)
 
@@ -151,5 +152,16 @@ def A(b, I ,g ,n):
     return A 
 
 
-#print(A(1,1,1,7))
+B=np.linspace(1,100,20)
+E=[]
+for b in B:
+    Pfaff=np.array(A(b,1,2,7))
+    e0=eig(Pfaff)[0]
+    e0c=np.imag(e0)
+    e1=[x for x in e0c if x>=0]
+    E.append(np.sort(e1)[0])
 
+plt.plot(B,E,'ro')
+plt.plot(B,B*0,b)
+plt.ylim([-7,7])
+plt.show()
