0f8543477588002fdafcf05c6645189a5046f81f
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2024-03-19 23:51:33 +0530

    Add files via upload

--- Worm dynamics on decorated triangle.py
+++ Worm dynamics on decorated triangle.py
@@ -15,9 +15,16 @@ for y in range(m+1):
 
             if 1<=x<=n-1:
                 if (x+y)%2==0:
-                   A.append([[x,y,2],[x,y-1,2]])
+                   if 1<y<=m-1:
+                     A.append([[x,y,2],[x,y-1,2]])
+                   else:
+                     A.append([[x,y,2],[int((x+1)/2),y-1,2]])
+
                 else: 
-                   A.append([[x,y,2],[x,y+1,2]])
+                    if 1<=y<m-1:
+                      A.append([[x,y,2],[x,y+1,2]])
+                    else:
+                      A.append([[x,y,2],[int((x+1)/2),y+1,2]])
                 
 
                 if x==1:
@@ -34,15 +41,38 @@ for y in range(m+1):
                
             elif x==0:
 
-                A.append([[x,y,1],[x,y+1,3]])
-                A.append([[x,y,2],[x+1,y,1]])
-                A.append([[x,y,3],[x,y-1,1]])
+                if y==1:
+                    A.append([[x,y,1],[x,y+1,3]])
+                    A.append([[x,y,2],[x+1,y,1]])
+                    A.append([[x,y,3],[1,y-1,1]])
+
+                elif y==m-1:
+                    A.append([[x,y,1],[1,y+1,1]])
+                    A.append([[x,y,2],[x+1,y,1]])
+                    A.append([[x,y,3],[x,y-1,1]])
+
+                else:
+                    A.append([[x,y,1],[x,y+1,3]])
+                    A.append([[x,y,2],[x+1,y,1]])
+                    A.append([[x,y,3],[x,y-1,1]])
+
 
             else:
 
-                A.append([[x,y,1],[x,y+1,3]])
-                A.append([[x,y,2],[x-1,y,3]])
-                A.append([[x,y,3],[x,y-1,1]])
+                if y==1:
+                    A.append([[x,y,1],[x,y+1,3]])
+                    A.append([[x,y,2],[x-1,y,3]])
+                    A.append([[x,y,3],[n0-1,y-1,3]])
+
+                elif y==m-1:
+                    A.append([[x,y,1],[n0-1,y+1,3]])
+                    A.append([[x,y,2],[x-1,y,3]])
+                    A.append([[x,y,3],[x,y-1,1]])
+
+                else:
+                    A.append([[x,y,1],[x,y+1,3]])
+                    A.append([[x,y,2],[x-1,y,3]])
+                    A.append([[x,y,3],[x,y-1,1]])
 
         
     elif y==0:
@@ -90,16 +120,19 @@ for y in range(m+1):
                A.append([[x,y,1],[x-1,y,3]])
                A.append([[x,y,2],[2*x-1,y-1,2]])
                A.append([[x,y,3],[n,y-1,1]])
+
+
            
 def Enum(x,y,k):
     if y==0: 
        a=3*(x-1)+k
-    elif y==n:
-       a=3*n0+(2*m)*(n)*3+(x-1)*3+k
+    elif y==m:
+       a=3*n0+(2*m0)*(n+1)*3+(x-1)*3+k
     else: 
-       a=3*n0+(y-1)*(n)*3+3*x+k
-    
+       a=3*n0+(y-1)*(n+1)*3+3*x+k
+
     return a
+
 
 E=[]
 
@@ -107,6 +140,7 @@ for ed in A:
     a1,a2=ed[0],ed[1]
     c1,c2=Enum(a1[0],a1[1],a1[2]),Enum(a2[0],a2[1],a2[2])
     E.append((c1,c2))
+
 
 G=nx.Graph(E)
 G.add_edges_from(E)
