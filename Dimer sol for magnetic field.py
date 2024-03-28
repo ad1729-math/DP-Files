76bef4590cf6e37a81e59d013705a990ebad8f68
Alapan Das <82791449+ad1729-math@users.noreply.github.com>
2024-03-28 19:21:31 +0530

    Add files via upload

--- Dimer sol for magnetic field.py
+++ Dimer sol for magnetic field.py
@@ -8,6 +8,7 @@ from scipy.integrate import dblquad
 
 h0=10**-3
 x0,y0=0,0
+
 def Zl(b,J,h): 
     a=2**(-1/4)
     d,g,t,e=np.exp(b*J),np.exp(-b*J/2),a*np.exp(-b*h/4),np.sqrt(a)*np.exp(b*h/8)
@@ -72,34 +73,47 @@ a0=np.log(1+np.sqrt(2))/2
 # # plt.plot(B,E1,'g+')
 # plt.show() 
 
-def integrand(beta, h, J, phi1, phi2):
+
+def integrand1(beta,J,h,phi1,phi2):
+
+    term1 = -beta*16*np.exp(-4*beta*h) + 8*beta*np.exp(4*beta*h) + 8*beta*np.exp(4*beta*h)*np.cosh(2*beta*J)
+    term2 = -8*(np.cos(phi1) + np.cos(phi2))*(2*beta*np.exp(2*beta*h)*np.cosh(beta*J) + 2*beta*np.exp(-2*beta*h))
+    result=1/4*(term1+term2)
+
+    return result
+
+
+def integrand(beta, J, h, phi1, phi2):
 
     term1 = 8 + 4*np.exp(-4*beta*h) + 2*np.exp(4*beta*h) + 2*np.exp(4*beta*h)*np.cosh(2*beta*J)
     term2 = -8*(np.cos(phi1) + np.cos(phi2))*(np.exp(2*beta*h)*np.cosh(beta*J) - np.exp(-2*beta*h))
-    term3 = -4*(np.cos(phi1 - phi2) + np.cos(phi1 + phi2))*(np.cos(beta*J) - 1)
+    term3 = -4*(np.cos(phi1 - phi2) + np.cos(phi1 + phi2))*(np.cosh(beta*J)-1)
     result=1/4*(term1+term2+term3)
-    return np.log(result)
+
+    r1=integrand1(beta,J,h,phi1,phi2)
+
+    return result
 
 
 def Z(beta, J, h):
     integrand_wrapper = lambda phi1, phi2: integrand(beta, J, h, phi1, phi2)
     integral, _ = dblquad(integrand_wrapper, 0, 2*np.pi, lambda x: 0, lambda x: 2*np.pi)
-    return 1/(8*np.pi**2)*integral
+    return 1/(8*np.pi**2)*integral/(2*beta)
 
 
 # Example usage:
-B=np.linspace(0.1,2,500)
-h=10**-6
+B=np.linspace(0.1,3,200)
+h=0
 E=[]
 for b in B:
-    a=(Z(b,1,h)-Z(b,1,0))/h
+    a=integrand(b,1,h,0,0)
     E.append(a)
 
 plt.plot(B,E,'g')
 plt.axvline(x=a0, color='b', linestyle='--')
+plt.axhline(y=0, color='r', linestyle='-')
 plt.xlabel("$1/T$ --->")
 plt.ylabel("Magnetization --->")
-plt.legend()
 plt.show()
 
 
